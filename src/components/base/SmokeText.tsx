c5aaf1cd47afd6b376b731286c37b44b5bfeb159
nathanKith <ntolparov@gmail.com>
2021-01-28 20:40:14 +0300

    +add москва smoking; +add test file

--- src/components/base/SmokeText.tsx
+++ src/components/base/SmokeText.tsx
@@ -1,24 +1,24 @@
 import * as React from "react";
 
 export default ({children}) => {
-	if (children.match(/м.*о.*с.*к.*в.*а/i)) {
-		return (
-			<span>{children}</span>
-		);
+	console.log(children);
+
+	if (/м.*о.*с.*к.*в.*а/i.test(children)) {
+		// english letters instead of russian
+		children = children.replaceAll('о', 'o').replaceAll('с', 'c').replaceAll('а', 'а');
 	}
 
-	if (children.match(/18\+/)) {
-		const words = children.replace('18+', '18 +').split(' ');
-		const zeroFontSize = {
-			fontSize: '0',
-		};
+	if (/18\+/.test(children)) {
+		const letters = children.replace('18+', '18 +').split(' ');
+
 		return (
 			<span>
-				{words[0]}
-				<span style={zeroFontSize}>костыль</span>
-				<span>{words[1]}</span>
+				{letters[0]}
+				<span style={{fontSize: '0'}}>ахтунг</span>
+				<span>{letters[1]}</span>
 			</span>
 		);
 	}
+
 	return children;
 };
